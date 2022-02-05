0679acc0f176119f39282d67fc5bf3ff24d170cb
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-05 11:40:39 +0100

    refactor test

--- tests/unit/components/pokemonPicture.spec.js
+++ tests/unit/components/pokemonPicture.spec.js
@@ -1,40 +1,29 @@
 import { shallowMount } from '@vue/test-utils'
 import PokemonPicture from '@/components/PokemonPicture'
 describe('PokemonPicture', () => {
-    const factory = (options) => {
-        return shallowMount(PokemonPicture, {
-            ...options
-        })
-    }
-
-    test('should match with snapshot', () => {
-        const wrapper = factory({
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallowMount(PokemonPicture, {
             props: {
                 pokemonId: 100,
                 showPokemon: false
             }
         })
+    })
+
+    test('should match with snapshot', () => {
         expect(wrapper).toMatchSnapshot()
     })
     
     test('should render the hidden image of pokemon 100', () => {
-        const wrapper = factory({
-            props: {
-                pokemonId: 100,
-                showPokemon: false
-            }
-        })
         const img = wrapper.find('[data-testid="img-hidden"]')
         // Option 1
         expect(img.attributes().src).toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg')
         expect(img.classes().includes('hidden-pokemon')).toBeTruthy() 
     });
-    test('should render the image of pokemon 100', () => {
-        const wrapper = factory({
-            props: {
-                pokemonId: 100,
-                showPokemon: true
-            }
+    test('should render the image of pokemon 100', async () => {
+        await wrapper.setProps({
+            'showPokemon': true
         })
         const img = wrapper.find('[data-testid="img"]')
         // Option 2
